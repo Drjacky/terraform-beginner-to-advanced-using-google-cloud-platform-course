60a07497974c9024a6a91bf0be6b6fe6a5710a5a
Drjacky <dr.jacky.2005@gmail.com>
2025-10-31 15:40:53 +0000

    Kubernetes

--- kube/main.tf
+++ kube/main.tf
@@ -1,3 +1,12 @@
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = "snappy-boulder-476716-s6"
@@ -11,24 +20,42 @@ module "gke" {
   http_load_balancing        = false
   horizontal_pod_autoscaling = true
   network_policy             = false
+  #filestore_csi_driver       = false
+  #dns_cache                  = false
 
   node_pools = [
     {
-      name               = "default-node-pool"
-      machine_type       = "n1-standard-2"
-      min_count          = 1
-      max_count          = 2 # quota errors if the number is too high
-      disk_size_gb       = 10
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
+      name         = "default-node-pool"
+      machine_type = "n1-standard-2"
+      #node_locations              = "us-central1-b,us-central1-c"
+      min_count = 1
+      max_count = 2 # quota errors if the number is too high
+      #local_ssd_count             = 0
+      #spot                        = false
+      disk_size_gb = 12
+      disk_type    = "pd-standard"
+      image_type   = "COS_CONTAINERD"
+      #enable_gcfs                 = false
+      #enable_gvnic                = false
+      #logging_variant             = "DEFAULT"
+      auto_repair  = true
+      auto_upgrade = true
+      #service_account    = "project-service-account@snappy-boulder-476716-s6.iam.gserviceaccount.com"
       preemptible        = false
       initial_node_count = 1
+      #accelerator_count           = 1
+      #accelerator_type            = "nvidia-l4"
+      #gpu_driver_version          = "LATEST"
+      #gpu_sharing_strategy        = "TIME_SHARING"
+      #max_shared_clients_per_gpu = 2
     },
   ]
 
   node_pools_oauth_scopes = {
+    /*all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]*/
     all = []
 
     default-node-pool = [
